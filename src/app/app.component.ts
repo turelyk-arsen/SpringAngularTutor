611ad7308cff23af190965006d1b5edfee33e5c5
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-11-20 18:53:10 +0100

    first connect to Spring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'SpringAngularTutor';
+ // greeting = {'id':'XXX', 'content': 'Hello world'};
+ greeting: any = {};
+ 
+ constructor(private http:HttpClient) {
+	 http.get('http://127.0.0.1:8080/resource', { withCredentials: true }).subscribe((data:any) => this.greeting = data,
+	 (error: any) => console.error('Error:', error));
+ }
 }
